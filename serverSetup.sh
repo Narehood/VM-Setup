93e9a3281eb720c2f9ef35bb3d6eac0298b4bbdc
Michael <Narehood@users.noreply.github.com>
2024-06-10 18:26:05 -0400

    Update serverSetup.sh
    Added support for Suse and Arch & reset directory after dotfiles install.

--- serverSetup.sh
+++ serverSetup.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-# Check if the system is Debian or Red Hat based
+# Check if the system is Debian, Ubuntu, Red Hat, Arch, or SUSE based
 if [ -f /etc/debian_version ]; then
-    # Debian-based system
+    # Debian-based system (including Ubuntu)
     sudo apt-get update
     sudo apt-get install -y xe-guest-utilities net-tools cockpit htop
 elif [ -f /etc/redhat-release ]; then
-    # Red Hat-based system
+    # Red Hat-based system (including Fedora)
     sudo yum update
     sudo yum install -y xe-guest-utilities net-tools cockpit htop
+elif [ -f /etc/arch-release ]; then
+    # Arch-based system
+    sudo pacman -Syu --noconfirm xe-guest-utilities net-tools cockpit htop
+elif [ -f /etc/SuSE-release ]; then
+    # SUSE-based system
+    sudo zypper refresh
+    sudo zypper install -y xe-guest-utilities net-tools cockpit htop
 else
     echo "Unsupported system. Exiting."
     exit 1
@@ -26,8 +33,8 @@ fi
 # Clone the dotfiles repository
 git clone https://github.com/Narehood/dotfiles.git
 cd dotfiles
-
 # Run the install.sh script
 bash install.sh
 su
 su
+cd
