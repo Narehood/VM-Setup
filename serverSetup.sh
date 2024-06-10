b75153756a122fac313583ff830fc09e352af77c
Michael <Narehood@users.noreply.github.com>
2024-06-10 18:10:50 -0400

    Update serverSetup.sh
    Made script slightly more robust

--- serverSetup.sh
+++ serverSetup.sh
@@ -1,9 +1,33 @@
-#/bin/sh
-apt update
-apt install xe-guest-utilities net-tools cockpit htop
-apt upgrade -y
+#!/bin/bash
 
+# Check if the system is Debian or Red Hat based
+if [ -f /etc/debian_version ]; then
+    # Debian-based system
+    sudo apt-get update
+    sudo apt-get install -y xe-guest-utilities net-tools cockpit htop
+elif [ -f /etc/redhat-release ]; then
+    # Red Hat-based system
+    sudo yum update
+    sudo yum install -y xe-guest-utilities net-tools cockpit htop
+else
+    echo "Unsupported system. Exiting."
+    exit 1
+fi
+
+# Ask the user if they want to change the hostname
+read -p "Do you want to change the hostname? (y/n): " change_hostname
+if [ "$change_hostname" == "y" ]; then
+    read -p "Enter the new hostname: " new_hostname
+    sudo hostnamectl set-hostname "$new_hostname"
+    echo "Hostname changed to $new_hostname."
+fi
+
+
+# Clone the dotfiles repository
 git clone https://github.com/Narehood/dotfiles.git
 cd dotfiles
-./install.sh
-sh
+
+# Run the install.sh script
+bash install.sh
+su
+su
