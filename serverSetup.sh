7ab61dfb557e4a78a3ac7c2bc7548eea6b8e30eb
Michael <Narehood@users.noreply.github.com>
2024-06-10 12:58:06 -0400

    Update serverSetup.sh
    Added htop

--- serverSetup.sh
+++ serverSetup.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 apt update
-apt install xe-guest-utilities net-tools cockpit
+apt install xe-guest-utilities net-tools cockpit htop
 apt upgrade -y
 
 git clone https://github.com/Narehood/dotfiles.git
