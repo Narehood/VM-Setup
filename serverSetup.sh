df13e6cc51a3513d3069f5a0fe3d88a81b3340b0
Michael <Narehood@users.noreply.github.com>
2024-06-10 12:33:59 -0400

    Update serverSetup.sh
    Fixing a typo in the dotfiles URL.

--- serverSetup.sh
+++ serverSetup.sh
@@ -3,6 +3,6 @@ apt update
 apt install xe-guest-utilities git net-tools
 apt upgrade -y
 
-git clone https://github.com/Narehood/dotfile.git
+git clone https://github.com/Narehood/dotfiles.git
 cd dotfile
 ./install.sh
