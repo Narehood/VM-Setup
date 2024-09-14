e499b775823f44d143b8cf601b80d08db3fbb6e6
Michael <Narehood@users.noreply.github.com>
2024-09-14 11:33:02 -0400

    Update serverSetup.sh

--- serverSetup.sh
+++ serverSetup.sh
@@ -4,28 +4,36 @@
 read -p "Do you want to install XCP-NG Tools? (XCP-NG Server Client) (y/n): " install_xen_tools
 if [ "$install_xen_tools" == "y" ]; then
 
-# Check if the system is Debian, Ubuntu, Red Hat, Arch, or SUSE based
-if [ -f /etc/debian_version ]; then
-    # Debian-based system (including Ubuntu)
-    sudo apt update -y
-    sudo apt install -y xe-guest-utilities
-elif [ -f /etc/redhat-release ]; then
-    # Red Hat-based system (including Fedora)
-    sudo yum update -y
-    sudo yum install -y xe-guest-utilities
-    sudo dnf update -y
-    sudo dnf install -y xe-guest-utilities
-elif [ -f /etc/arch-release ]; then
-    # Arch-based system
-    sudo pacman -Syu --noconfirm xe-guest-utilities
-elif [ -f /etc/SuSE-release ]; then
-    # SUSE-based system
-    sudo zypper refresh
-    sudo zypper install -y xe-guest-utilities
-else
-    echo "Unsupported system. Exiting."
-    exit 1
+    # Check if the system is Debian, Ubuntu, Red Hat, Arch, or SUSE based
+    if [ -f /etc/debian_version ]; then
+        if [ -f /etc/lsb-release ] && grep -q 'Ubuntu' /etc/lsb-release; then
+            # Ubuntu system
+            sudo apt update -y
+            sudo apt install -y xe-guest-utilities
+        else
+            # Debian system
+            sudo apt update -y
+            sudo apt install -y xe-guest-utilities
+        fi
+    elif [ -f /etc/redhat-release ]; then
+        # Red Hat-based system (including Fedora)
+        sudo yum update -y
+        sudo yum install -y xe-guest-utilities
+        sudo dnf update -y
+        sudo dnf install -y xe-guest-utilities
+    elif [ -f /etc/arch-release ]; then
+        # Arch-based system
+        sudo pacman -Syu --noconfirm xe-guest-utilities
+    elif [ -f /etc/SuSE-release ]; then
+        # SUSE-based system
+        sudo zypper refresh
+        sudo zypper install -y xe-guest-utilities
+    else
+        echo "Unsupported system. Exiting."
+        exit 1
+    fi
 fi
+
 
 # Install Standard Server Tools
     
