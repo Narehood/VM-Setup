257e019db3dc81532a9774487d50471125e65cf5
Michael <Narehood@users.noreply.github.com>
2024-09-23 17:03:00 -0400

    Create Docker-Prep.sh

---
+++ Installers/Docker-Prep.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Clone the XenOrchestraInstallerUpdater repository
+git clone https://github.com/Narehood/Docker-Prep
+cd Docker-Prep
+
+# Run the installation script
+bash install.sh
