dfe6ab5be84bb52cb1963d883c90059a47a98829
Michael <Narehood@users.noreply.github.com>
2024-09-23 16:19:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ check_for_updates() {
     local remote_file="https://raw.githubusercontent.com/Narehood/VM-Setup/main/install.sh"
 
     # Download the remote file to a temporary location
-    https://github.com/Narehood/VM-Setup/main/install.sh /tmp/install.sh $remote_file
+    wget https://github.com/Narehood/VM-Setup/main/install.sh /tmp/install.sh $remote_file
 
     # Compare the local and remote files
     if ! cmp -s $local_file /tmp/install.sh; then
