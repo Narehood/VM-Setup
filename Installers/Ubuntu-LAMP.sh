94ecd249adbd4a53e278907d46536fea8851a785
Michael <Narehood@users.noreply.github.com>
2024-10-12 15:51:28 -0400

    Create Ubuntu-LAMP.sh

---
+++ Installers/Ubuntu-LAMP.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Get the server's IP address
+SERVER_IP=$(hostname -I | awk '{print $1}')
+
+# Update package list
+echo "Updating package list..."
+sudo apt update
+
+# Install Apache
+echo "Installing Apache..."
+sudo apt install -y apache2
+
+# Check Apache status
+echo "Checking Apache status..."
+sudo systemctl status apache2
+
+# Install MySQL
+echo "Installing MySQL..."
+sudo apt install -y mysql-server
+
+# Secure MySQL
+echo "Securing MySQL installation..."
+sudo mysql_secure_installation
+
+# Install PHP
+echo "Installing PHP..."
+sudo apt install -y php libapache2-mod-php php-mysql
+
+# Restart Apache
+echo "Restarting Apache..."
+sudo systemctl restart apache2
+
+# Open HTTP (port 80) and HTTPS (port 443) ports
+echo "Configuring firewall to allow HTTP and HTTPS traffic..."
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw reload
+
+# Print completion message
+echo "LAMP stack installation complete! You can check PHP info at http://$SERVER_IP"
