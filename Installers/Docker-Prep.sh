554252f8d79e4f875846c6a67b088e798e6b23d2
Michael <Narehood@users.noreply.github.com>
2024-10-12 13:25:08 -0400

    Update Docker-Prep.sh

--- Installers/Docker-Prep.sh
+++ Installers/Docker-Prep.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 # Clone the XenOrchestraInstallerUpdater repository
-git clone https://github.com/Narehood/Docker-Prep
-cd Docker-Prep
+REPO_URL="https://github.com/Narehood/Docker-Prep"
+DEST_DIR="Docker-Prep"
+
+if [ -d "$DEST_DIR" ] || [ -f "$DEST_DIR" ]; then
+    read -p "$DEST_DIR already exists. Do you want to replace it? (y/n): " REPLACE
+    if [ "$REPLACE" != "y" ]; then
+        echo "Operation aborted."
+        exit 1
+    fi
+    rm -rf "$DEST_DIR"
+fi
+
+git clone "$REPO_URL"
+cd "$DEST_DIR"
 
 # Run the installation script
 bash install.sh
