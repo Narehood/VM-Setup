0647586b3d11aecbd95d4c89d7fd2ef46f087a84
Michael <Narehood@users.noreply.github.com>
2024-10-13 20:03:35 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
 
-# Function to display the menu
-show_menu() {
-    clear
-    echo -e "====================================="
-    echo -e "          \e[1;34mVM Setup Menu 1.2.2\e[0m"
-    echo -e "====================================="
-    echo -e "\e[1;32mSystem Information\e[0m"
-    echo -e "-------------------------------------"
-    echo -e "Linux Distribution: \e[1;33m$(lsb_release -d | cut -f2)\e[0m"
-    echo -e "Kernel Version: \e[1;33m$(uname -r)\e[0m"
-    echo -e "CPU Usage: \e[1;33m$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')\e[0m"
-    echo -e "Memory Usage: \e[1;33m$(free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }')\e[0m"
-    echo -e "Disk Usage: \e[1;33m$(df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}')\e[0m"
-    echo -e "-------------------------------------"
-    echo -e "\e[1;32mOptions\e[0m"
-    echo -e "-------------------------------------"
-    echo -e "\e[1;36m1.\e[0m XCP-NG / Virtual Machine Initial Configuration"
-    echo -e "\e[1;36m2.\e[0m Xen Orchestra"
-    echo -e "\e[1;36m3.\e[0m UniFi Controller"
-    echo -e "\e[1;36m4.\e[0m Docker Host Prep"
-    echo -e "\e[1;36m5.\e[0m Enable Automated Security Patches"
-    echo -e "\e[1;36m6.\e[0m Check for Updates"
-    echo -e "\e[1;36m7.\e[0m Exit"
-    echo -e "====================================="
-}
-
 # Function to check for updates in the repository
 check_for_updates() {
     echo "Checking for updates in VM-Setup repository..."
@@ -46,6 +20,35 @@ check_for_updates() {
             echo "Update aborted."
         fi
     fi
+}
+
+# Check for updates at launch
+check_for_updates
+
+# Function to display the menu
+show_menu() {
+    clear
+    echo -e "====================================="
+    echo -e "          \e[1;34mVM Setup Menu 1.2.3\e[0m"
+    echo -e "====================================="
+    echo -e "\e[1;32mSystem Information\e[0m"
+    echo -e "-------------------------------------"
+    echo -e "Linux Distribution: \e[1;33m$(lsb_release -d | cut -f2)\e[0m"
+    echo -e "Kernel Version: \e[1;33m$(uname -r)\e[0m"
+    echo -e "CPU Usage: \e[1;33m$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')\e[0m"
+    echo -e "Memory Usage: \e[1;33m$(free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }')\e[0m"
+    echo -e "Disk Usage: \e[1;33m$(df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}')\e[0m"
+    echo -e "-------------------------------------"
+    echo -e "\e[1;32mOptions\e[0m"
+    echo -e "-------------------------------------"
+    echo -e "\e[1;36m1.\e[0m XCP-NG / Virtual Machine Initial Configuration"
+    echo -e "\e[1;36m2.\e[0m Xen Orchestra"
+    echo -e "\e[1;36m3.\e[0m UniFi Controller"
+    echo -e "\e[1;36m4.\e[0m Docker Host Prep"
+    echo -e "\e[1;36m5.\e[0m Enable Automated Security Patches"
+    echo -e "\e[1;36m6.\e[0m Check for Updates"
+    echo -e "\e[1;36m7.\e[0m Exit"
+    echo -e "====================================="
 }
 
 # Main loop
