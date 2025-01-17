c94b0228944a2facb0730ec3ef82d07800dbb672
Michael <Narehood@users.noreply.github.com>
2025-01-17 12:56:19 -0500

    Update XenOrchestra.sh

--- Installers/XenOrchestra.sh
+++ Installers/XenOrchestra.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# Define the directory and repository URL
+repo_dir="XenOrchestraInstallerUpdater"
+repo_url="https://github.com/Narehood/XenOrchestraInstallerUpdater"
+
+# Check if the directory exists
+if [ -d "$repo_dir" ]; then
+    # Remove the directory and its contents
+    rm -rf "$repo_dir"
+fi
+
 # Clone the XenOrchestraInstallerUpdater repository to the home directory of the current user
 cd
-git clone https://github.com/Narehood/XenOrchestraInstallerUpdater
-cd XenOrchestraInstallerUpdater
+git clone "$repo_url"
+cd "$repo_dir"
 
 # Run the installation script
 bash xo-install.sh
