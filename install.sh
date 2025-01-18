c586526ee2a784a4afcad61c64ccd61a4fe9c82f
Michael <Narehood@users.noreply.github.com>
2025-01-18 12:48:23 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 show_menu() {
     clear
     echo -e "====================================="
-    echo -e "          \e[1;34mVM Setup Menu 1.5.8\e[0m"
+    echo -e "          \e[1;34mVM Setup Menu 1.5.9\e[0m"
     echo -e "====================================="
     echo -e "\e[1;32mResource Information\e[0m"
     echo -e "-------------------------------------"
@@ -98,7 +98,6 @@ while true; do
             ;;
         8)
             echo "Exiting..."
-            exit
             exit 0
             ;;
         *)
