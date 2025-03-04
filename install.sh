4d6d5984f18e99ec8935abd9761aa17363851c81
Michael <Narehood@users.noreply.github.com>
2025-03-04 16:51:25 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 show_menu() {
     clear
     echo -e "\033[0;32m=====================================\033[0m"
-    echo -e "          \033[1;34mVM Setup Menu 1.7.10\033[0m"
+    echo -e "          \033[1;34mVM Setup Menu 2.0.0\033[0m"
     echo -e "\033[0;32m=====================================\033[0m"
     echo -e "\033[1;32mResource Information\033[0m"
     echo -e "-------------------------------------"
@@ -23,13 +23,11 @@ show_menu() {
     echo -e "\033[1;32mOptions\033[0m"
     echo -e "-------------------------------------"
     echo -e "\033[1;36m1.\033[0m XCP-NG / Virtual Machine Initial Configuration"
-    echo -e "\033[1;36m2.\033[0m Xen Orchestra"
-    echo -e "\033[1;36m3.\033[0m UniFi Controller"
-    echo -e "\033[1;36m4.\033[0m Docker Host Prep"
-    echo -e "\033[1;36m5.\033[0m Other Installers"
-    echo -e "\033[1;36m6.\033[0m Enable Automated Security Patches"
-    echo -e "\033[1;36m7.\033[0m Check for System Updates"
-    echo -e "\033[1;36m8.\033[0m Check for Menu Updates"
+    echo -e "\033[1;36m2.\033[0m Installer Scripts"
+    echo -e "\033[1;36m3.\033[0m Docker Host Prep"
+    echo -e "\033[1;36m4.\033[0m Enable Automated Security Patches"
+    echo -e "\033[1;36m5.\033[0m Check for System Updates"
+    echo -e "\033[1;36m6.\033[0m Check for Menu Updates"
     echo -e "\033[1;36m9.\033[0m Exit"
     echo -e "\033[0;32m=====================================\033[0m"
 }
@@ -65,37 +63,29 @@ execute_installerScript() {
 # Main loop
 while true; do
     show_menu
-    read -p "Enter your choice [1-8]: " choice
+    read -p "Enter your choice [1-6]: " choice
     case $choice in
         1)
             echo "You have selected XCP-NG / Virtual Machine Initial Configuration"
             execute_installerScript "serverSetup.sh"
             ;;
         2)
-            echo "You have selected Xen Orchestra"
-            execute_installerScript "XenOrchestra.sh"
+            echo "You have selected Other Installers"
+            execute_installerScript "installer.sh"
             ;;
         3)
-            echo "You have selected UniFi Controller"
-            execute_installerScript "UniFi-Controller.sh"
-            ;;
-        4)
             echo "You have selected Docker Host Prep"
             execute_installerScript "Docker-Prep.sh"
             ;;
-        5)
-            echo "You have selected Other Installers"
-            execute_installerScript "installerMenu.sh"
-            ;;
-        6)
+        4)
             echo "You have selected Enable Automated Security Patches"
             execute_installerScript "Automated-Security-Patches.sh"
             ;;
-        7)
+        5)
             echo "You have selected System Update"
             execute_installerScript "systemUpdate.sh"
             ;;
-        8)
+        6)
             echo "Checking for updates..."
             check_for_updates
             ;;
@@ -107,7 +97,7 @@ while true; do
             echo -e "\033[0;31mInvalid option. Please choose a number between 1 and 8.\033[0m"
             ;;
     esac
-    if [ "$choice" -ne 8 ]; then
+    if [ "$choice" -ne 9 ]; then
         read -p "Press [Enter] key to return to menu or type 'exit' to exit: " next_action
         if [ "$next_action" = "exit" ]; then
             exit 0
