0a718791daaf6abe969d5a362d268fe47d4a7a8b
Michael <Narehood@users.noreply.github.com>
2025-03-04 16:53:15 -0500

    Update installer.sh

--- Installers/installer.sh
+++ Installers/installer.sh
@@ -4,7 +4,7 @@
 show_menu() {
     clear
     echo -e "\033[0;32m=====================================\033[0m"
-    echo -e "          \033[1;34mVM Setup Menu\033[0m"
+    echo -e "          \033[1;34mVM Setup Installer Menu\033[0m"
     echo -e "\033[0;32m=====================================\033[0m"
     echo -e "\033[1;32mResource Information\033[0m"
     echo -e "-------------------------------------"
@@ -22,14 +22,9 @@ show_menu() {
     echo -e "-------------------------------------"
     echo -e "\033[1;32mOptions\033[0m"
     echo -e "-------------------------------------"
-    echo -e "\033[1;36m1.\033[0m XCP-NG / Virtual Machine Initial Configuration"
+    echo -e "\033[1;36m1.\033[0m WordPress"
     echo -e "\033[1;36m2.\033[0m Xen Orchestra"
     echo -e "\033[1;36m3.\033[0m UniFi Controller"
-    echo -e "\033[1;36m4.\033[0m Docker Host Prep"
-    echo -e "\033[1;36m5.\033[0m Other Installers"
-    echo -e "\033[1;36m6.\033[0m Enable Automated Security Patches"
-    echo -e "\033[1;36m7.\033[0m Check for System Updates"
-    echo -e "\033[1;36m8.\033[0m Check for Menu Updates"
     echo -e "\033[1;36m9.\033[0m Exit"
     echo -e "\033[0;32m=====================================\033[0m"
 }
@@ -43,10 +38,10 @@ execute_installerScript() {
 # Main loop
 while true; do
     show_menu
-    read -p "Enter your choice [1-8]: " choice
+    read -p "Enter your choice [1-3]: " choice
     case $choice in
         1)
-            echo "You have selected XCP-NG / Virtual Machine Initial Configuration"
+            echo "You have selected WordPress"
             execute_installerScript "WordPress.sh"
             ;;
         2)
