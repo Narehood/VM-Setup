a8aacfab704ce73ac818305f70d7fd2cdfae3997
Michael <Narehood@users.noreply.github.com>
2025-09-03 11:20:42 -0400

    Update serverSetup.sh

--- Installers/serverSetup.sh
+++ Installers/serverSetup.sh
@@ -140,7 +140,7 @@ arch)
 debian | ubuntu)
   sudo apt update -y
   sudo apt upgrade -y
-  sudo apt install -y net-tools btop plocate whois
+  sudo apt install -y net-tools btop plocate whois neofetch
   ;;
 fedora)
   sudo dnf update -y
