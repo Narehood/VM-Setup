4bca0f14c5f7a534c3817d06d1b5f8d74793a1ce
Michael <Narehood@users.noreply.github.com>
2025-09-11 10:26:17 -0400

    Create linutil.sh

---
+++ Installers/linutil.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Credit to Chris Titus Tech for this script: https://github.com/ChrisTitusTech/linutil
+# The script will do most of what my script does, plus a lot more.
+
+curl -fsSL christitus.com/linux | sh
